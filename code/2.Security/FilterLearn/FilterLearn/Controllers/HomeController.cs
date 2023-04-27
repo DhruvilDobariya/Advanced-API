6010a2df0b0b431c11e683f680c17e17fe3b3298
dhruvil.d <190540107051@darshan.ac.in>
2023-04-27 18:29:29 +0530

    Learn action filter

---
+++ code/2.Security/FilterLearn/FilterLearn/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using FilterLearn.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace FilterLearn.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [ActionFilterLearn]
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage()
+            {
+                // don't use this, "HttpContext.Current.Request.Headers.Get("X-Id")" because, it give vanilla request, it don't give that request object which we should modify using filter. 
+                Content = new StringContent(Request.Headers.GetValues("X-Id").FirstOrDefault()),
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+    }
+}
---
+++ code/2.Security/FilterLearn/FilterLearn/Filters/ActionFilterLearn.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace FilterLearn.Filters
+{
+    //public class ActionFilterLearn : ActionFilterAttribute
+    //{
+    //    public override void OnActionExecuting(HttpActionContext actionContext)
+    //    {
+    //        base.OnActionExecuting(actionContext);
+    //        actionContext.Request.Headers.Add("X-Id", "1");
+    //    }
+    //    public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+    //    {
+    //        base.OnActionExecuted(actionExecutedContext);
+    //        actionExecutedContext.Response.Headers.Add("X-Name", "Dhruvil Dobariya");
+    //    }
+
+    //    // Asynchronous filter
+    //    public override async Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+    //    {
+    //        actionContext.Request.Headers.Add("X-Id", "1");
+    //        await base.OnActionExecutingAsync(actionContext, cancellationToken);
+    //    }
+    //    public override async Task OnActionExecutedAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
+    //    {
+    //        actionExecutedContext.Response.Headers.Add("X-Name", "Dhruvil Dobariya");
+    //        await base.OnActionExecutedAsync(actionExecutedContext, cancellationToken);
+    //    }
+    //}
+    public class ActionFilterLearn : Attribute, IActionFilter
+    {
+        public bool AllowMultiple => true;
+
+        public async Task<HttpResponseMessage> ExecuteActionFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
+        {
+            actionContext.Request.Headers.Add("X-Id", "1");
+            var response = await continuation();
+            response.Headers.Add("X-Name", "Dhruvil A. Dobariya");
+            return response;
+        }
+    }
+}
