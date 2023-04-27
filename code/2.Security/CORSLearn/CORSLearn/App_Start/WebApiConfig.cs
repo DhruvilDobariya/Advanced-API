e83d6380172ff071f7c2bb6dd3ad7c9e35e62b64
dhruvil.d <190540107051@darshan.ac.in>
2023-04-27 16:06:54 +0530

    Learn CORS

---
+++ code/2.Security/CORSLearn/CORSLearn/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace CORSLearn
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // CORS configuration
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*", "X-Id, X-Name");
+            config.EnableCors(cors);
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ code/2.Security/CORSLearn/CORSLearn/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace CORSLearn.Controllers
+{
+    [EnableCors(origins: "www.something1.com, www.somthing2.com", headers: "X-Header1, X-Header2", methods: "GET,PUT,POST,DELETE", exposedHeaders: "X-Id,X-Name")]
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [EnableCors(origins:"*", headers: "*", methods: "GET", exposedHeaders:"X-Name")]
+        public HttpResponseMessage Get(int id, string name)
+        {
+            HttpContext.Current.Response.Headers.Add("X-Id", id.ToString());
+            HttpContext.Current.Response.Headers.Add("X-Name", name);
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(new {id,name})),
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+    }
+}
